2c426855db2cd5de3ec8a3d59995d4437ceb28e8
tshifhiwa <matombot@gmail.com>
2021-07-30 06:52:18 +0200

    appointments added

--- migrations/006-patient-data.sql
+++ migrations/006-patient-data.sql
@@ -4,4 +4,3 @@ insert into patients (id_number,first_name,last_name,contact_no) values (2985689
 insert into patients (id_number,first_name,last_name,contact_no) values (2985233338665,'Nelson','','Simunye'0786444333');
 insert into patients (id_number,first_name,last_name,contact_no) values (2985777808665,'Bongani','Dlamini','0815689004');
 insert into patients (id_number,first_name,last_name,contact_no) values (2985888808665,'Themba','Mabuza','0815689004');
--- insert into doctors (first_name,last_name,email,shift_type,rate) values('Trevor','Nkosini', 'tn@gmail.com','evening',100);
